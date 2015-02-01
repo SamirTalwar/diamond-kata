744963e50fdfec033c9b12cd2ccd2485242ae1e9
Samir Talwar <samir@noodlesandwich.com>
2015-02-01 13:43:11 +0000

    Delete the first test, as it's covered by a later one.

--- DiamondTest.hs
+++ DiamondTest.hs
@@ -14,9 +14,7 @@ nonEmpty = filter (/= ' ')
 
 main = do
     mapM_ quickCheck
-        [(label "The first cell in the biggest row of the diamond is the largest"
-             (\size -> [head (diamond size !! (fromEnum size - 1))] == (show size))),
-         (label "The diamond has 2*N-1 rows, where N is the size"
+        [(label "The diamond has 2*N-1 rows, where N is the size"
              (\size -> (length $ diamond size) == fromEnum size * 2 - 1)),
          (label "The diamond has 2*N-1 columns, where N is the size"
              (\size -> all (\row -> length row == fromEnum size * 2 - 1) (diamond size))),
