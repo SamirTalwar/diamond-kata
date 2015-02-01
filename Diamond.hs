fa5d86b2c01d742b82fa454c3eff3eae04292f75
Samir Talwar <samir@noodlesandwich.com>
2015-02-01 11:15:07 +0000

    The first cell in the biggest row of the diamond is the largest.

--- Diamond.hs
+++ Diamond.hs
@@ -1,7 +1,10 @@
 module Diamond where
 
 data DiamondSize = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
-    deriving (Eq, Read, Show)
+    deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+sizes :: [DiamondSize]
+sizes = [minBound..maxBound]
 
 diamond :: DiamondSize -> [[Char]]
-diamond _ = undefined
+diamond _ = map show sizes
--- DiamondTest.hs
+++ DiamondTest.hs
@@ -1,9 +1,17 @@
 module Main where
 
 import Diamond
+
 import Test.Hspec
+import Test.QuickCheck
+
+instance Arbitrary DiamondSize where
+    arbitrary = elements sizes
 
 main = do
+    quickCheck $ (label "The first cell in the biggest row of the diamond is the largest"
+                   (\size -> [head (diamond size !! fromEnum size)] == (show size)))
+
     hspec $ do
         describe "Diamond" $ do
             it "works" $ do
